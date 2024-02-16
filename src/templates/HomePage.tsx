33330c92bd975e1e26e9d8904fa560026f4ef4d7
Raheal Safdar <mrahealsafdar@gmail.com>
2024-02-17 03:58:51 +0500

    setup next.js project

---
+++ src/templates/HomePage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      Hello World, This is my Porftolio Website
+    </div>
+  );
+};
+
+export default HomePage;
